999d4ef7683cf8ae4876541febecbefedb921c7f
Myst0g4n <ikkituoderschvanck@gmail.com>
2025-02-22 21:34:20 +0900

    Menambahkan code untuk memanggil fungsi pada utils

--- struktur-data/m_iqbal_r_mauw/main.go
+++ struktur-data/m_iqbal_r_mauw/main.go
@@ -6,8 +6,12 @@ import (
 
 // main function
 func main() {
-	utils.Array()  // panggil fungsi Array
-	utils.Slice()  // panggil fungsi Slice
-	utils.Map()    // panggil fungsi Map
-	utils.Struct() // panggil fungsi Struct
+	utils.Array()      // panggil fungsi Array
+	utils.Slice()      // panggil fungsi Slice
+	utils.Map()        // panggil fungsi Map
+	utils.Struct()     // panggil fungsi Struct
+	utils.Pointer()    // panggil fungsi Pointer
+	utils.Channel()    // panggil fungsi Channel
+	utils.Interface()  // panggil fungsi Interface
+	utils.LinkedList() // panggil fungsi LinkedList
 }
